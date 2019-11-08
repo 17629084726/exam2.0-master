c57458f23b159da66c1590b8d68c6536b2705abe
javaduxin <dx8802667@163.com>
2019-11-08 17:28:58 +0800

    2019.11.8下午下班前提交

--- page/news/examInit.js
+++ page/news/examInit.js
@@ -5,26 +5,8 @@ layui.use(['form','layer','jquery'],function(){
 		$ = layui.jquery;
 
  	var systemParameter;
- 	form.on("submit(systemParameter)",function(data){
- 		systemParameter = '{"cmsName":"'+$(".cmsName").val()+'",';  //模版名称
- 		systemParameter += '"version":"'+$(".version").val()+'",';	 //当前版本
- 		systemParameter += '"author":"'+$(".author").val()+'",'; //开发作者
- 		systemParameter += '"homePage":"'+$(".homePage").val()+'",'; //网站首页
- 		systemParameter += '"server":"'+$(".server").val()+'",'; //服务器环境
- 		systemParameter += '"dataBase":"'+$(".dataBase").val()+'",'; //数据库版本
- 		systemParameter += '"maxUpload":"'+$(".maxUpload").val()+'",'; //最大上传限制
- 		systemParameter += '"userRights":"'+$(".userRights").val()+'",'; //用户权限
- 		systemParameter += '"description":"'+$(".description").val()+'",'; //站点描述
- 		systemParameter += '"powerby":"'+$(".powerby").val()+'",'; //版权信息
- 		systemParameter += '"record":"'+$(".record").val()+'",'; //网站备案号
- 		systemParameter += '"keywords":"'+$(".keywords").val()+'"}'; //默认关键字
- 		window.sessionStorage.setItem("systemParameter",systemParameter);
- 		//弹出loading
- 		var index = top.layer.msg('数据提交中，请稍候',{icon: 16,time:false,shade:0.8});
-        setTimeout(function(){
-            layer.close(index);
-			layer.msg("系统基本参数修改成功！");
-        },500);
+ 	form.on("submit",function(data){
+ 		window.location.href = "exam.html";
  		return false;
  	})
 
